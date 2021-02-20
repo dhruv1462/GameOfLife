bb8d4bac57531a799e45e23adcff1e81ab09b876
DHRUV PATEL <45648070+dhruv1462@users.noreply.github.com>
2021-02-19 20:30:33 -0700

    Update CellGeneration.java

--- ConwaysGameOfLife/src/CellGeneration.java
+++ ConwaysGameOfLife/src/CellGeneration.java
@@ -1,49 +1,55 @@
 import java.awt.*;
 import java.util.ArrayList;
 
+/**
+ * Represents the rules for the evolution of cells.
+ * @author Aayush Rawal (arawal2)
+ * @author Dennis Polly Pynadath (dpynadat)
+ * @author Saurabh Rane (ssrane)
+ * @author Dhruv Dilipkumar Patel (dpatel81)
+ * @author Sameet Krishnakumar (skris114)
+ */
 public class CellGeneration {
-    public ArrayList<Point> evolve(boolean[][] gameBoard, ArrayList<Point> point) {
+
+     ArrayList<Point> evolve(boolean[][] gameBoard, ArrayList<Point> point) {
         for (Point current : point) {
             gameBoard[current.x + 1][current.y + 1] = true;
         }
         ArrayList<Point> survivingCells = new ArrayList<Point>(0);
-        // Iterate through the array, follow game of life rules
         for (int i = 1; i < gameBoard.length - 1; i++) {
             for (int j = 1; j < gameBoard[0].length - 1; j++) {
-                int surrounding = 0;
+                int neighborCell = 0;
                 if (gameBoard[i - 1][j - 1]) {
-                    surrounding++;
+                    neighborCell++;
                 }
                 if (gameBoard[i + 1][j - 1]) {
-                    surrounding++;
+                    neighborCell++;
                 }
                 if (gameBoard[i - 1][j + 1]) {
-                    surrounding++;
+                    neighborCell++;
                 }
                 if (gameBoard[i][j - 1]) {
-                    surrounding++;
+                    neighborCell++;
                 }
                 if (gameBoard[i - 1][j]) {
-                    surrounding++;
+                    neighborCell++;
                 }
                 if (gameBoard[i][j + 1]) {
-                    surrounding++;
+                    neighborCell++;
                 }
                 if (gameBoard[i + 1][j + 1]) {
-                    surrounding++;
+                    neighborCell++;
                 }
                 if (gameBoard[i + 1][j]) {
-                    surrounding++;
+                    neighborCell++;
                 }
 
                 if (gameBoard[i][j]) {
-                    // Cell is alive, Can the cell live? (2-3)
-                    if ((surrounding == 2) || (surrounding == 3)) {
+                    if ((neighborCell == 2) || (neighborCell == 3)) {
                         survivingCells.add(new Point(i - 1, j - 1));
                     }
                 } else {
-                    // Cell is dead, will the cell be given birth? (3)
-                    if (surrounding == 3) {
+                    if (neighborCell == 3) {
                         survivingCells.add(new Point(i - 1, j - 1));
                     }
                 }
